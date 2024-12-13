591b2fc7f7317459e878d97bbea1ff186118908f
hyeshinchu <67092706+hyeshinchu@users.noreply.github.com>
2024-12-13 20:09:27 +0900

    Update 3_🌏_CV.py

--- pages/3_🌏_CV.py
+++ pages/3_🌏_CV.py
@@ -21,7 +21,7 @@ with body:
         def show_pdf(file_path):
             with open(file_path, "rb") as f:
                 base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf">'
+            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="800" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
 
         # PDF 파일 표시
